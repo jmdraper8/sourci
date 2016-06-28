c18990ad8084060be9f36cf95360c2efa77482ae
John Draper <johndraper@icloud.com>
2016-06-28 23:44:06 +1000

    Merged db, and app.js

--- server.js
+++ server.js
@@ -3,9 +3,38 @@ var express = require('express');
 var app = express();
 var http = require('http').Server(app);
 var io = require('socket.io')(http);
+var bodyParser = require('body-parser');
+var _ = require('underscore');
+var db = require('./db.js');
+var bcrypt = require('bcrypt');
+var middleware = require('./middleware.js')(db);
+
+var todos = [];
+var todoNextId = 1;
+
+app.use(bodyParser.json());
+
 
 app.use(express.static(__dirname + '/public'));
 
-http.listen(PORT, function () {
-	console.log('Server started on port: ' + PORT);
+//POST /users/
+app.post('/register', function (req, res) {
+
+	console.log("register users");
+
+	// var body = _.pick(req.body, 'email', 'password');
+
+	// db.user.create(body).then(function (users) {
+	// 	res.json(users.toPublicJSON());
+	// }, function (e) {
+	// 	res.status(400).json(e);
+	// });
+
 });
+
+db.sequelize.sync({force: true}).then(function () {
+	app.listen(PORT, function () {
+	console.log('Server started on port: ' + PORT);
+	});
+});
+
